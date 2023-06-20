64be0ad85d10eacfe5951fd5247663d0600ef5ce
pengland1 <97199861+pengland1@users.noreply.github.com>
2023-06-20 17:26:23 -0600

    Added functionality
    - Added some settings to allow quick editing of odds and command name & description.
    - Punched in some additional comments
    - Wait and permission responses are now hidden and will disappear after 15 seconds
    - Fixed bug with permission response that would crash the bot.

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import discord, json, random, time, math, os
 from discord import app_commands
 from dotenv import load_dotenv
 
+#   Load environment variables from .env file
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+
+#   Setting up the Discord Intents
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+
+#   Setting up the command tree for slash commands
 tree = app_commands.CommandTree(client)
 
 #   Regular Functions
 def getLowestTimeSpan():
+    """
+    Function to find the lowest time span in the config.json file.
+    """
+    #   Open the config.json file and load the data into a variable
     file = open("config.json")
     configData = json.load(file)
     file.close()
+
+    #   Loop through the roles in the config.json file and find the lowest time span
     index = 0
     timespan = 0
     for role in configData["roles"]:
@@ -24,6 +35,7 @@ def getLowestTimeSpan():
     return timespan
 
 def isAccessible(userRoles):
+    
     file = open("config.json")
     configData = json.load(file)
     file.close()
@@ -34,67 +46,110 @@ def isAccessible(userRoles):
     return False
 
 def getRandomResponse(responseType, rawTimeStampText):
+    """
+    Function to get a random response from the config.json file and replace the {timeStamp} with the appropriate timestamp text.
+    """
+    #   Parse the rawTimeStampText from a string into a JSON object
     timeStamp = json.loads(rawTimeStampText)
+
+    #   Open the config.json file and load the data into a variable
     configFile = open("config.json")
     configData = json.load(configFile)
     configFile.close()
+
+    #   Select random response
     responseLength = len(configData["responses"][responseType])
     randomChoice = random.randint(1,responseLength - 1)
+
+    #   Replace {timeStamp} with the appropriate timestamp text
     timeStampText = ""
     if responseType == "wait":
         timeStampText = f"<t:{math.trunc(timeStamp['userTime'] + timeStamp['timeSpan'])}:R>"
     else:
         timeStampText = f"<t:{math.trunc(time.time() + timeStamp['timeSpan'])}:R>"
-        
     return configData["responses"][responseType][str(randomChoice)].replace("{timeStamp}", (timeStampText))
+
+def getRandomResponse(responseType):
+    """
+    Function (overload, so same as above) to get a random response from the config.json file without timestamp text.
+    """
+    configFile = open("config.json")
+    configData = json.load(configFile)
+    configFile.close()
+    responseLength = len(configData["responses"][responseType])
+    randomChoice = random.randint(1,responseLength - 1)
+    return configData["responses"][responseType][str(randomChoice)]
+
+def getSettings():
+    """
+    Function to read settings from config.json
+    """
+    file = open("config.json")
+    configData = json.load(file)
+    file.close()
+    return configData["settings"]
 
 #  Events
 @client.event
 async def on_ready():
+    """
+    Event function that is called when the bot has connected to Discord
+    """
     await tree.sync()
-    print("Ready!")
+    print(f'{client.user} has connected to Discord!')
 
 #   Slash Commands
-@tree.command(name = "warp", description = "Make a warp wish to the Cosmic Council, and the energies of the universe may grant you a boon.")
+@tree.command(name = getSettings()["command_name"], description = getSettings()["command_description"])
 async def warp_command(interaction):
+    """
+    Slash command function for the warp command
+    """
+    # Get time span and check if user has access
     timeSpan = getLowestTimeSpan()
     hasAccess = isAccessible(interaction.user.roles)
 
+    #   If user does not have access, send a message saying so
     if not hasAccess:
         print(f"User {interaction.user.id} prompted permission response.")
-        await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponse('permission')}")
+        await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponse('permission')}", ephemeral = True, delete_after = 15)
     else:
         isReturningUser = False
         isWithinHour = False
+
+        #   Open the warpJournal.json file and load the data into a variable
         file = open("warpJournal.json")
         data = json.load(file)
         file.close()
         indexOfExistingUser = 0
 
+        #   Loop through the warpJournal.json file and check if the user has warped within the last hour
+        #   Also check if the user has warped before
         for entry in data["warpHistory"]:
             userID = entry["id"]
             userTime = entry["time"]
             if userID == interaction.user.id:
                 isReturningUser = True
+                #   If the user has already warped within their timeSpan, send a message saying so
                 if time.time() - userTime < timeSpan:
                     print(f"User {interaction.user.id} prompted wait response.")
                     timeStampJSON = "{\"userTime\": " + f"{userTime}" + ", \"timeSpan\": " + f"{timeSpan}" + "}"
-                    await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponse('wait', timeStampJSON)}")
+                    await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponse('wait', timeStampJSON)}", ephemeral = True, delete_after = 15)
                     isWithinHour = True
                     break
                 else:
                     break
             indexOfExistingUser += 1
 
+        #   If the user has not warped within the last hour, roll the dice and send a response
         if not isWithinHour:
             currentTime = time.time()
-            roll = random.randint(0, 500)
+            roll = random.randint(0, getSettings()["odds"])
 
             if(roll == 1):
                 print(f"User {interaction.user.id} prompted win response.")
                 timeStampJSON = "{\"userTime\": " + f"{currentTime}" + ", \"timeSpan\": " + f"{timeSpan}" + "}"
                 await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponse('win', timeStampJSON)}")
-                
+            
             else:
                 print(f"User {interaction.user.id} prompted lose response.")
                 timeStampJSON = "{\"userTime\": " + f"{currentTime}" + ", \"timeSpan\": " + f"{timeSpan}" + "}"
