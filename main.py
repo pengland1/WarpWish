28d2c287341602c906960a1b813351af8b96bbc1
pengland1 <97199861+pengland1@users.noreply.github.com>
2023-07-08 17:45:13 -0600

    In progress warp log command
     - Added regions for better readability
     - Added location to warps to add some fun
     - warpjournal now contains the results and date/time from every roll a user has done
     - beta of warplog added. This command just displays recent history and stats of their past warps.
     - complete revamp of how the system logs data.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import discord, json, random, time, math, os
+import discord, json, random, time, math, os, calendar
 from discord import app_commands
 from dotenv import load_dotenv
+from datetime import datetime
 
+#region Setup
 #   Load environment variables from .env file
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
@@ -12,9 +14,10 @@ client = discord.Client(intents=intents)
 
 #   Setting up the command tree for slash commands
 tree = app_commands.CommandTree(client)
+#endregion
 
-#   Regular Functions
-def getLowestTimeSpan():
+#region Regular Functions
+def getLowestTimeSpan(userRoles):
     """
     Function to find the lowest time span in the config.json file.
     """
@@ -23,15 +26,19 @@ def getLowestTimeSpan():
     configData = json.load(file)
     file.close()
 
+    # Extract the IDs from userRoles into a set for efficient lookup
+    userRoleIds = set(str(role.id) for role in userRoles)
+
     #   Loop through the roles in the config.json file and find the lowest time span
-    index = 0
-    timespan = 0
+    timespan = None
     for role in configData["roles"]:
-        if index == 0:
-            timespan = int(role["time_span"])
-        if int(role["time_span"]) < timespan:
-            timespan = int(role["time_span"])
-        index = index + 1
+        # Check if the user has this role by comparing IDs
+        if str(role["id"]) in userRoleIds:
+            # If this is the first role we're checking, or the timespan is lower than the current lowest timespan, update the lowest timespan.
+            if timespan == None:
+                timespan = int(role["time_span"])
+            if int(role["time_span"]) < timespan:
+                timespan = int(role["time_span"])
     return timespan
 
 def getFirstAvailableTimespan(userRoles):
@@ -93,6 +100,17 @@ def getRandomResponse(responseType):
     randomChoice = random.randint(1,responseLength - 1)
     return configData["responses"][responseType][str(randomChoice)]
 
+def getRandomWarpLocation():
+    """
+    Function to get a random warp location from the config.json file.
+    """
+    configFile = open("config.json")
+    configData = json.load(configFile)
+    configFile.close()
+    responseLength = len(configData["warp_locations"])
+    randomChoice = random.randint(1, responseLength - 1)
+    return configData["warp_locations"][randomChoice]
+
 def getSettings():
     """
     Function to read settings from config.json
@@ -102,7 +120,56 @@ def getSettings():
     file.close()
     return configData["settings"]
 
-#  Events
+def getRollHistory(userID):
+    """
+    Function to get the roll history of a user from the warpJournal.json file
+    """
+    file = open("warpJournal.json")
+    data = json.load(file)
+    file.close()
+    for entry in data["warpHistory"]:
+        if entry["id"] == userID:
+            if "roll_history" not in entry:
+                return []
+            else:
+                return entry["roll_history"]
+    return None
+
+def getUserWinSum(userID):
+    """
+    Function to get the sum of all wins of a user from the warpJournal.json file
+    """
+    wins = 0
+    for roll in getRollHistory(userID):
+        if roll["warp_result"] == "win":
+            wins = wins + 1
+    return wins
+
+def convert_seconds(total_seconds):
+    """
+    Convert a number of seconds into hours, minutes, and seconds.
+
+    :param total_seconds: int, The total number of seconds
+    :return: tuple, A tuple containing hours, minutes, and seconds
+    """
+
+    # Calculate hours
+    hours = total_seconds // 3600
+
+    # Calculate the remaining seconds after the hours are accounted for
+    remaining_seconds = total_seconds % 3600
+
+    # Calculate minutes
+    minutes = remaining_seconds // 60
+
+    # Calculate the remaining seconds after the minutes are accounted for
+    seconds = remaining_seconds % 60
+
+    return hours, minutes, seconds
+#endregion
+
+#region Events
+
 @client.event
 async def on_ready():
     """
@@ -110,8 +177,44 @@ async def on_ready():
     """
     await tree.sync()
     print(f'{client.user} has connected to Discord!')
+#endregion
 
-#   Slash Commands
+#region Slash Commands
+@tree.command(name = getSettings()["log_command_name"], description = getSettings()["log_command_description"])
+async def warpLog_command(interaction):
+    """
+    Slash command function for the warpLog command. Shows the user's lottery history.
+    """
+    userID = interaction.user.id
+    history = getRollHistory(userID)
+    wins = 0 if len(history) == 0 else getUserWinSum(userID)
+    cooldownHours, cooldownMinutes, cooldownSeconds = convert_seconds(getLowestTimeSpan(interaction.user.roles))
+    cooldownString = f"{cooldownHours} hours, {cooldownMinutes} minutes, and {cooldownSeconds} seconds"
+
+    embedVar = discord.Embed(title=f":rocket: Warp Log", description="----------------------------", color=0x00ff00)
+    embedVar.add_field(name=f":chart_with_upwards_trend: Total Warps: ```{len(history)}```", value="" , inline=False)
+
+    if len(history) > 0:
+        latestWarp = history[len(history) - 1]
+
+        embedVar.add_field(name=f":white_check_mark: Wins: ```{0 if wins == None else wins}```", value="", inline=False)
+        embedVar.add_field(name=f":hourglass: Cooldown ```{cooldownString}```", value="", inline=False)
+        embedVar.add_field(name=f"\n", value="", inline=False)
+        embedVar.add_field(name=f"\nLatest Warp", value="----------------------------", inline=False)
+        if "date" in latestWarp:
+            latestWarpDate = datetime.strptime(latestWarp["date"] + " " + latestWarp["time_rolled"], "%m-%d-%Y %H:%M:%S")
+            embedVar.add_field(name=f":calendar_spiral: Date: ```{calendar.month_name[latestWarpDate.month]} {latestWarpDate.day}, {latestWarpDate.year}```", value="" , inline=False)
+            embedVar.add_field(name=f":clock: Time: <t:{int(time.mktime(latestWarpDate.timetuple()))}:R>", value="" , inline=False)
+        if "location" in latestWarp:
+            latestWarpLocation = latestWarp["location"]
+            embedVar.add_field(name=f":round_pushpin: Location: ```{latestWarpLocation}```", value="" , inline=False)
+        if "warp_result" in latestWarp:
+            latestWarpResult = "Failure" if latestWarp["warp_result"] == "lose" else "Success"
+            embedVar.add_field(name=f":trophy: Result: ```{latestWarpResult}```", value="" , inline=False)
+
+    embedVar.set_thumbnail(url=interaction.user.display_avatar)
+    await interaction.response.send_message(embed=embedVar)
+
 @tree.command(name = getSettings()["command_name"], description = getSettings()["command_description"])
 async def warp_command(interaction):
     """
@@ -167,7 +270,7 @@ async def warp_command(interaction):
                 print(f"User {interaction.user.id} prompted lose response.")
                 timeStampJSON = "{\"userTime\": " + f"{currentTime}" + ", \"timeSpan\": " + f"{timeSpan}" + "}"
                 await interaction.response.send_message(f"<@{interaction.user.id}> - {getRandomResponseTimestamp('lose', timeStampJSON)}")
-                
+            """ OLD CODE, BUT KEEPING FOR TESTING / REFERENCE
             # Define function to write dictionary data into a json format
             def writeToWarpJournal(file_data, tempDict):
                 if isReturningUser:
@@ -193,7 +296,57 @@ async def warp_command(interaction):
                 "warp_result": "win" if roll == 1 else "lose",
                 "times_rolled": 1
             }
+            """
+            # Define function to write dictionary data into a json format
+            def writeToWarpJournal(file_data, tempDict):
+                if isReturningUser:
+                    file_data["warpHistory"][indexOfExistingUser].update({"time": time.time()})
+                    if "roll_history" in file_data["warpHistory"][indexOfExistingUser]:
+                        file_data["warpHistory"][indexOfExistingUser]["roll_history"].append({"date": time.strftime("%m-%d-%Y", time.localtime(currentTime)),
+                                                                                                "time_rolled": time.strftime("%H:%M:%S", time.localtime(currentTime)),
+                                                                                                "warp_result": "win" if roll == 1 else "lose",
+                                                                                                "location": getRandomWarpLocation()["name"]
+                                                                                            })
+                    else:
+                        file_data["warpHistory"][indexOfExistingUser].update({"roll_history": [
+                                {
+                                    "date": time.strftime("%m-%d-%Y", time.localtime(currentTime)),
+                                    "time_rolled": time.strftime("%H:%M:%S", time.localtime(currentTime)),
+                                    "warp_result": "win" if roll == 1 else "lose",
+                                    "location": getRandomWarpLocation()["name"]
+                                }
+                            ]
+                        })
+                        if "date" in file_data["warpHistory"][indexOfExistingUser]:
+                            del file_data["warpHistory"][indexOfExistingUser]["date"]
+                        if "time_rolled" in file_data["warpHistory"][indexOfExistingUser]:
+                            del file_data["warpHistory"][indexOfExistingUser]["time_rolled"]
+                        if "warp_result" in file_data["warpHistory"][indexOfExistingUser]:
+                            del file_data["warpHistory"][indexOfExistingUser]["warp_result"]
+                        if "times_rolled" in file_data["warpHistory"][indexOfExistingUser]:
+                            del file_data["warpHistory"][indexOfExistingUser]["times_rolled"]
+                else:
+                    file_data["warpHistory"].append(tempDict)
+                with open("warpJournal.json", "w") as file:  # write file
+                    json.dump(file_data, file, indent = 4)
+
+            # Create a dictionary to insert into the json file
+            tempDict = {
+                "id": interaction.user.id,
+                "time": currentTime,
+                "name": interaction.user.name,
+                "discriminator": interaction.user.discriminator,
+                "roll_history": [
+                    {
+                        "date": time.strftime("%m-%d-%Y", time.localtime(currentTime)),
+                        "time_rolled": time.strftime("%H:%M:%S", time.localtime(currentTime)),
+                        "warp_result": "win" if roll == 1 else "lose",
+                        "location": getRandomWarpLocation()["name"]
+                    }
+                ]
+            }
             writeToWarpJournal(data, tempDict)
         file.close()
+#endregion
 
 client.run(TOKEN)
